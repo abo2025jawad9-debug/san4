3daa396a9af32efd4bf7ee527ccb34060380f287
abo2025jawad9-debug <abo2025jawad9@gmail.com>
2026-08-07 23:50:59 +0300

    Change trading symbol from AAVEUSDT to CRCLXUSDT

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ API_SECRET = cfg.secret
 TELEGRAM_TOKEN = cfg.telegram_token
 TELEGRAM_CHAT_ID = cfg.telegram_chat_id
 
-SYMBOL = "AAVEUSDT"
+SYMBOL = "CRCLXUSDT"
 BUY_AMOUNT_USD = 14.3
 TAKER_FEE_PERCENT = 0.001
 MIN_PROFIT_USD = 0.05  
